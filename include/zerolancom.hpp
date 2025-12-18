fd8776fe881bab48b3579e062806b70e20bbd3b0
Xinkai Jiang <jiangxinkai98@gmail.com>
2025-12-18 13:01:01 +0100

    add zlc based function

--- include/zerolancom.hpp
+++ include/zerolancom.hpp
@@ -9,3 +9,44 @@
 #include <zmq.hpp>
 #include <spdlog/spdlog.h>
 #include <msgpack.hpp>
+
+namespace zlc {
+
+void init(const std::string& node_name, const std::string& ip_address)
+{
+    zlc::ZeroLanComNode::init(node_name, ip_address);
+}
+
+template <typename HandlerT>
+void registerServiceHandler(
+    const std::string& service_name,
+    const std::function<std::string(const std::string&)>& handler)
+{
+    zlc::ZeroLanComNode::instance().registerServiceHandler<std::string, std::string>(service_name, handler);
+}
+
+template <typename HandlerT, typename ClassT>
+void registerServiceHandler(
+    const std::string& service_name,
+    HandlerT handler,
+    ClassT* instance)
+{
+    zlc::ZeroLanComNode::instance().registerServiceHandler<std::string, std::string>(service_name, std::bind(handler, instance));
+}
+
+template <typename HandlerT>
+void registerSubscriberHandler(const std::string& name, HandlerT handler)
+{
+    zlc::ZeroLanComNode::instance().registerSubscriber<std::string>(name, handler);
+}
+
+template <typename HandlerT, typename ClassT>
+void registerServiceHandler(
+    const std::string& service_name,
+    HandlerT handler,
+    ClassT* instance)
+{
+    zlc::ZeroLanComNode::instance().registerServiceHandler<std::string, std::string>(service_name, std::bind(handler, instance));
+}
+
+}
